0bb6ebbd3504fec88210c6d948b5eb6b2b73e811
DESKTOP-MMEUCNO\admin <trannguyen2004ns@gmail.com>
2024-03-19 20:20:26 +0700

    gggggggg

--- 3_PL/Program.cs
+++ 3_PL/Program.cs
@@ -7,7 +7,7 @@ namespace _3_PL
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
+            
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new FrmLogin());
